7b12bed416504151967e554f13ce4d7fff5779ed
Marc Balemboy <marc.balemboy@gmail.com>
2013-01-20 23:25:05 +0100

    README rempli

--- projetSIABD/Controllers/HomeController.cs
+++ projetSIABD/Controllers/HomeController.cs
@@ -9,9 +9,12 @@ namespace projetSIABD.Controllers
     [HandleError]
     public class HomeController : Controller
     {
+        private MySQLEntities db = new MySQLEntities();
+
         public ActionResult Index()
         {
             ViewData["Message"] = "Bienvenu sur le réseau social de l'entreprise *******";
+            my_aspnet_users moi = db.my_aspnet_users.Where(a => a.name.Equals(User.Identity.Name)).FirstOrDefault();
 
             return View();
         }
